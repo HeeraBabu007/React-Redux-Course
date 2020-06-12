2999c9242ba1278a449750c420d00f697bed20db
Heera Babu <heerababu80@gmail.com>
2020-06-12 23:34:59 +0530

    applyMiddleware in redux
    C:\Users\HeeraBabu\Documents\redux>git checkout -b applyMiddleware
    Switched to a new branch 'applyMiddleware'
    
    C:\Users\HeeraBabu\Documents\redux>git add .
    
    C:\Users\HeeraBabu\Documents\redux>git add .
    
    C:\Users\HeeraBabu\Documents\redux>git commit -m "logging"
    On branch applyMiddleware
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
      (commit or discard the untracked or modified content in submodules)
            modified:   nodejs-redux (modified content)
    
    no changes added to commit (use "git add" and/or "git commit -a")
    
    C:\Users\HeeraBabu\Documents\redux>git push -u origin applyMiddleware
    fatal: 'origin' does not appear to be a git repository
    fatal: Could not read from remote repository.
    
    Please make sure you have the correct access rights
    and the repository exists.
    
    C:\Users\HeeraBabu\Documents\redux>

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const redux = require('redux');
-const createStore = redux.createStore;
-const initialState = { numberOfBooks: 10 }
-function buyBook() { return { type: "Buy_Book", info: "My First Redux Program" } }
-const Reducer = (state = initialState, action) => {
+const {redux,createStore, combineReducers, applyMiddleware} = require('redux');
+
+const bookInitialState = { numberOfBooks: 10}
+const penInitialState = { numberOfPen: 20 }
+function buyBook() { return { type: "Buy_Book", payload: "My First Action for Redux App" } }
+function buyPen() { return { type: "Buy_Pen", payload: "My Second Action for Redux App" } }
+const BookReducer = (state = bookInitialState, action) => {
     switch (action.type) {
         case "Buy_Book": return {
             ...state,
@@ -11,10 +13,36 @@ const Reducer = (state = initialState, action) => {
         default: return state;
     }
 }
-const store = createStore(Reducer);
+
+const PenReducer = (state = penInitialState, action) => {
+    switch (action.type) {
+       case "Buy_Pen": return {
+            ...state,
+            numberOfPen: state.numberOfPen - 1
+        }
+        default: return state;
+    }
+}
+const reducer=combineReducers({
+    book:BookReducer,
+    pen:PenReducer
+});
+
+const logger=store=>{
+    return next=>{
+        return action=>{
+        const result=next(action);
+        console.log("middleware log",result)
+        return result;
+        }
+    }
+}
+ 
+const store = createStore(reducer,applyMiddleware(logger));
 console.log("Initial State Value: ", store.getState());
 const unsubscribe = store.subscribe = () => {
     console.log("Update State Value: ", store.getState());
 };
 store.dispatch(buyBook());
+store.dispatch(buyPen());
 unsubscribe();
